d2d2f63a51d279369194d78bbb6e62d01d9dec58
Mateo-Rey <jrey3darts@gmail.com>
2022-09-01 07:53:47 -0400

    project component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
-import { Card, Button, Alert } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card, Button, Alert, Icon } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { Project } from "./Project";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const history = useNavigate();
+  const AddProject = ( ) => {
+    useEffect(() => {
+      fetch()
+      
+    },[])
+  }
 
   async function handleLogout() {
     setError("");
@@ -23,128 +30,25 @@ export default function Dashboard() {
   return (
     <>
       <div className="app">
-          {error && <Alert variant="danger">{error}</Alert>}
+        {error && <Alert variant="danger">{error}</Alert>}
         <Sidebar handleLogout={handleLogout} error={error} />
+        <div className="hero">
+          <div className="tab-bar w-100px bg-blue background-re h-100px visible">
+            <button className="add-project"></button>
+            <button className="update-project"></button>
+            <button className="search-bar"></button>
+          </div>
+        </div>
         <div className="">
           <Card className="outer-project-frame position-absolute bg-blue text-white border-b-4 border-r-4 border-white">
-            <h2 className="text-xl">Welcome, UserName</h2>
+            <h2 className="text-xl">Welcome, Strawberry</h2>
             <Card.Body className="project-board ">
-              <Card className="project-frame border-3 bg-transparent">
-                <Card.Body className="singular-project leading-7 bg-tapue-gray border-4 border-white">
-                  <h3>Project Title</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                </Card.Body>
-              </Card>
-              <Card className="project-frame border-3 leading-7 bg-transparent">
-                <Card.Body className="singular-project bg-tapue-gray border-4 border-white">
-                  <h3>Project Title</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                </Card.Body>
-              </Card>
-              <Card className="project-frame border-3 bg-transparent">
-                <Card.Body className="singular-project leading-7 bg-tapue-gray border-4 border-white">
-                  <h3>Project Title</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                </Card.Body>
-              </Card>
-              <Card className="project-frame border-3 bg-transparent">
-                <Card.Body className="singular-project leading-7 bg-tapue-gray border-4 border-white">
-                  <h3>Project Title</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nulla sit odit, deserunt quas accusamus magni itaque
-                    suscipit aperiam in nobis tenetur blanditiis! Iure pariatur
-                    unde consequuntur dolor quis, eaque sed?
-                  </p>
-                </Card.Body>
-              </Card>
+              <Project/>
+              <Project/>
+              <Project/>
+              <Project/>
+              <Project/>
+              <Project/>
             </Card.Body>
           </Card>
         </div>
--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,50 +1,50 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function UpdateProfile() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const { currentUser, updatePassword, updateEmail } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
-  const history = useNavigate()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { currentUser, updatePassword, updateEmail } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useNavigate();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
+      return setError("Passwords do not match");
     }
 
-    const promises = []
-    setLoading(true)
-    setError("")
+    const promises = [];
+    setLoading(true);
+    setError("");
 
     if (emailRef.current.value !== currentUser.email) {
-      promises.push(updateEmail(emailRef.current.value))
+      promises.push(updateEmail(emailRef.current.value));
     }
     if (passwordRef.current.value) {
-      promises.push(updatePassword(passwordRef.current.value))
+      promises.push(updatePassword(passwordRef.current.value));
     }
 
     Promise.all(promises)
       .then(() => {
-        history('/')
+        history("/");
       })
       .catch(() => {
-        setError("Failed to update account")
+        setError("Failed to update account");
       })
       .finally(() => {
-        setLoading(false)
-      })
+        setLoading(false);
+      });
   }
 
   return (
     <>
-      <Card >
-        <Card.Body >
+      <Card className="my-5 bg-gray text-white">
+        <Card.Body>
           <h2 className="text-center mb-4">Update Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
@@ -59,27 +59,25 @@ export default function UpdateProfile() {
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-              />
+              <Form.Control type="password" ref={passwordRef} />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-              />
+              <Form.Control type="password" ref={passwordConfirmRef} />
             </Form.Group>
-            <button disabled={loading} className=" w-100 bg-blue text-white font-bold py-2 px-4 border-b-4 border-gray-light hover:border-gray rounded-3" type="submit">
+            <button
+              disabled={loading}
+              className=" w-100 my-2 bg-blue text-white font-bold py-2 px-4 border-b-4 border-gray-light hover:border-gray rounded-3"
+              type="submit"
+            >
               Update
             </button>
           </Form>
+          <div className="w-100 text-white text-center mt-2">
+            <Link to="/">Cancel</Link>
+          </div>
         </Card.Body>
       </Card>
-      <div className="w-100 text-white text-center mt-2">
-        <Link to="/">Cancel</Link>
-      </div>
     </>
-  )
+  );
 }
