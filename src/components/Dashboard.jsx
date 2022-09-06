c21db464412673c4977467e3ff5fa23273a776c3
Mateo-Rey <jrey3darts@gmail.com>
2022-09-06 17:45:30 -0400

    form is working, trying to put tasks into an array

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,33 +3,25 @@ import { Card, Button, Modal, Alert, Form, Icon } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import { Project } from "./Project";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
   const [show, setShow] = useState(false);
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [dataInit, setDataInit] = useState(false);
   const userId = 1;
-  const index = (Math.floor(Math.random()*100000))
 
-  const [form, setForm] = useState({})
-  const [newTask, setNewTask] = useState([{id:`${index}`}]);
+  const [form, setForm] = useState({});
+  const [newTask, setNewTask] = useState([{id: ''}]);
   const [projectData, setProjectData] = useState([]);
   const [addProject, setAddProject] = useState();
   const { currentUser, logout } = useAuth();
   const [updateProject, setUpdateProject] = useState();
- 
+
   const projectId = "l4wEImA53wUhOwmeEAKh";
   const history = useNavigate();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const handleGetFetch = () => {
-    fetch(`https://todo-api-web.web.app/projects/${userId}`)
-      .then((res) => res.json())
-      .then((data) => setProjectData(data))
-      .catch((err) => console.log(err));
-  };
 
   async function docAdd() {
     try {
@@ -40,16 +32,21 @@ export default function Dashboard() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(form), 
+          body: JSON.stringify(form),
         }
       );
       const data = results.json();
-      console.log(data);
+      window.location.reload(false);
     } catch (error) {
       console.error(error);
     }
   }
-
+  useEffect(() => {
+    fetch(`https://todo-api-web.web.app/projects/${userId}`)
+      .then((res) => res.json())
+      .then((data) => setProjectData(data))
+      .catch((err) => console.log(err));
+  }, []);
   async function handleLogout() {
     setError("");
 
@@ -61,7 +58,7 @@ export default function Dashboard() {
     }
   }
 
-  const addTask = () => {
+  const addTask = (index) => {
     
     if (newTask.length < 1) {
       return setNewTask({id:`${index}`})
@@ -71,20 +68,19 @@ export default function Dashboard() {
     }
     
   };
-  console.log(count)
 
   const handleChange = (event) => {
-    setForm({...form, [event.target.name]: event.target.value })
-  }
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
+  console.log(form);
   
-  console.log(form)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    docAdd(e)
+    docAdd(e);
   };
-  
+  console.log(newTask)
   return (
     <>
       <div className="app">
@@ -98,45 +94,73 @@ export default function Dashboard() {
             >
               Add Project
             </button>
-            <Modal className="flex justify-content-evenly align-items-center" show={show} onHide={handleClose}>
+            <Modal
+              className="flex justify-content-evenly align-items-center"
+              show={show}
+              onHide={handleClose}
+            >
               <Modal.Header closeButton>
                 <Modal.Title>Add New Project</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <form className="flex justify-content-between align-items-flex-start flex-md-column" onSubmit={handleSubmit}>
+                <form
+                  className="flex justify-content-between align-items-flex-start flex-md-column"
+                  onSubmit={(e) => e.preventDefault}
+                >
+                  <Form.Group>
+                    <Form.Label>User Id</Form.Label>
+                    <Form.Control
+                      onChange={handleChange}
+                      value={form.name}
+                      name="uuid"
+                      type="text"
+                      placeholder="Enter email..."
+                    />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group> 
                   <Form.Group className="mb-3">
                     <Form.Label className="">Project Title</Form.Label>
-                    <Form.Control onChange={handleChange} name='projTitle' type="text" placeholder="Enter title..." value={form.name} />
+                    <Form.Control
+                      onChange={handleChange}
+                      name="projectTitle"
+                      type="text"
+                      placeholder="Enter title..."
+                      value={form.name}
+                    />
                   </Form.Group>
-                  {newTask.map(() => {
-                    
+                  {newTask.map((item, index) => {
                     return (
-                    <Form.Group>
-                    <Form.Label>Project Task</Form.Label>
-                    <Form.Control onChange={handleChange} type="text" value={form.name} name={'task'+newTask[(0+count)].id} placeholder="Enter task..." />
-                  </Form.Group>
-                    )
-
+                      <Form.Group>
+                        <Form.Label>Project Task</Form.Label>
+                        <Form.Control
+            
+                          onChange={(e) => handleChange(e,index)}
+                          type="text"
+                          key={index}
+                          value={form.name}
+                          name={"task" + index}
+                          placeholder="Enter task..."
+                        />
+                      </Form.Group>
+                    );
                   })}
-                  
+
                   <button
+                    type="button"
                     className="mt-3 w-30 bg-blue text-white font-bold py-2 px-4 border-b-4 border-gray-light hover:border-gray rounded-3"
                     onClick={addTask}
                   >
                     Add Task
                   </button>
-                  <Form.Group>
-                    <Form.Label>User Id</Form.Label>
-                    <Form.Control onChange={handleChange} value={form.name} name='uuid' type="text" placeholder="Enter email..." />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
-                  </Form.Group>
                 </form>
               </Modal.Body>
               <Modal.Footer>
                 <button onClick={handleClose}>Close</button>
-                <button onClick={handleSubmit}type='submit'>Submit</button>
+                <button onClick={handleSubmit} type="submit">
+                  Submit
+                </button>
               </Modal.Footer>
             </Modal>
             <button
@@ -158,17 +182,27 @@ export default function Dashboard() {
           <Card className="outer-project-frame position-absolute bg-blue text-white border-b-4 border-r-4 border-white">
             <h2 className="text-xl">Welcome, Strawberry</h2>
             <Card.Body className="project-board ">
-              <Project />
-              <Project />
-              <Project />
-              <Project />
-              <Project />
-              <Project />
+              {projectData &&
+                projectData.map((doc) => {
+                  console.log(doc.activityList);
+                  let taskList = doc.activityList;
+                  return (
+                    <Card className="project-frame border-3 bg-transparent">
+                      <Card.Body className="singular-project leading-7 bg-tapue-gray border-4 border-white">
+                        <h3>{doc.projectTitle}</h3>
+                        {taskList &&
+                          taskList.map((doc) => {
+                            return <p>{doc}</p>;
+                          })}
+                        <p>{doc.userId}</p>
+                      </Card.Body>
+                    </Card>
+                  );
+                })}
             </Card.Body>
           </Card>
         </div>
       </div>
     </>
   );
-
 }
